16f399bb79c0aa7ea4d02c704eece1193c17550d
panduvallabhasetti <panduvallabhasetti96@gmail.com>
2024-08-06 22:57:38 +0530

    new groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,8 @@
-p stageParams) {
+def call(Map stageParams) {
  
-     checkout([
-          $class: 'GitSCM',
-	  branches: [[name:  stageParams.branch ]],
-	  userRemoteConfigs: [[ url: stageParams.url ]]
-              ])
-           }
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
